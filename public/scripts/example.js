83b434e02e810619eb3a8ce5c021677afba78637
Rupert Bedford <rupert@rupertb.com>
2016-04-23 19:13:28 +0100

    Add comment component

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,32 +1,62 @@
+var data = [
+  {id: 1, author: "Alice", text: "Ping!"},
+  {id: 2, author: "Bob", text: "**bold**"},
+];
+
 var CommentList = React.createClass({
-    render: function() {
-        return (
-            <div>Comment list!</div>
-        );
-    }
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
+    return (
+      <div>
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return {__html: rawMarkup};
+  },
+  render: function() {
+    return (
+      <div>
+        <h2>{this.props.author}</h2>
+        <div dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
 });
 
 var CommentForm = React.createClass({
-    render: function() {
-        return (
-            <div>Comment form!</div>
-        );
-    }
+  render: function() {
+    return (
+      <div>Comment form!</div>
+    );
+  }
 });
 
 var CommentBox = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <h1>Comments</h1>
-                <CommentList />
-                <CommentForm />
-            </div>
-        );
-    }
+  render: function() {
+    return (
+      <div>
+        <h1>Comments</h1>
+        <CommentList data={this.props.data} />
+        <CommentForm />
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(
-    <CommentBox />,
-    document.getElementById('content')
+  <CommentBox data={data} />,
+  document.getElementById('content')
 );
