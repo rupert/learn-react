ded9b7847b0d8d7521069b8daab640b051fe5bea
Rupert Bedford <rupert@rupertb.com>
2016-04-23 16:39:41 +0100

    Add components

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,4 +1,32 @@
+var CommentList = React.createClass({
+    render: function() {
+        return (
+            <div>Comment list!</div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div>Comment form!</div>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <CommentBox />,
     document.getElementById('content')
 );
