97dd600576c9ae167f698640a842bd6ed96a79dc
Rupert Bedford <rupert@rupertb.com>
2016-04-24 12:07:43 +0100

    Use ES6

---
+++ app.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import $ from 'jquery';
+import marked from 'marked';
+
+class CommentList extends React.Component {
+  render() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
+    return (
+      <div>
+        {commentNodes}
+      </div>
+    );
+  }
+}
+
+class Comment extends React.Component {
+  rawMarkup() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return {__html: rawMarkup};
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.author}</h2>
+        <div dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+}
+
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {author: '', text: ''};
+
+    this.handleAuthorChange = this.handleAuthorChange.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleAuthorChange(e) {
+    this.setState({author: e.target.value});
+  }
+
+  handleTextChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+
+    if (!text || !author) {
+      return;
+    }
+
+    this.props.onCommentSubmit({author: author, text: text});
+    this.setState({author: '', text: ''});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Comment"
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" />
+      </form>
+    );
+  }
+}
+
+export default class CommentBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: []};
+
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+  }
+
+  loadCommentsFromServer() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+
+  handleCommentSubmit(comment) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer.bind(this), this.props.pollInterval);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+      </div>
+    );
+  }
+}
